e6773b3e25021ccc6ad510192e6f8cdbe08dd385
yangux <yeonie05@gmail.com>
2020-10-17 17:42:53 +0900

    test: Check type

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const resultMsg = document.getElementById('resultMsg');
 const lowOrHigh = document.getElementById('lowOrHigh');
 const replayBtn = document.getElementById('replayBtn');
 
+let randomNum = Math.floor(Math.random() * 100) + 1;
+console.log(randomNum);
+
 init();
 function init() {
   submitBtn.addEventListener('click', checkAnswer);
@@ -12,7 +15,9 @@ function init() {
 }
 
 function checkAnswer() {
-
+  let guess = Number(inputText.value);
+  console.log(guess === randomNum);
+  console.log(typeof guess);
 }
 
 function replay() {
