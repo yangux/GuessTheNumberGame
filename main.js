38fbbba9c8b180d0b5c3c3d0dbde5925a79e981c
yangux <yeonie05@gmail.com>
2020-10-17 17:36:17 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+const inputText = document.getElementById('inputText');
+const submitBtn = document.getElementById('submitBtn');
+const preGuesses = document.getElementById('preGuesses');
+const resultMsg = document.getElementById('resultMsg');
+const lowOrHigh = document.getElementById('lowOrHigh');
+const replayBtn = document.getElementById('replayBtn');
+
+init();
+function init() {
+  submitBtn.addEventListener('click', checkAnswer);
+  replayBtn.addEventListener('click', replay);
+}
+
+function checkAnswer() {
+
+}
+
+function replay() {
+
+}
