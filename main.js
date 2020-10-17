bf952103928331ecc79b44cad459bb82f3499ab8
yangux <yeonie05@gmail.com>
2020-10-17 17:59:57 +0900

    feat: Add check answer

--- main.js
+++ main.js
@@ -8,16 +8,39 @@ const replayBtn = document.getElementById('replayBtn');
 let randomNum = Math.floor(Math.random() * 100) + 1;
 console.log(randomNum);
 
+let guesses = new Array();
+
 init();
 function init() {
   submitBtn.addEventListener('click', checkAnswer);
   replayBtn.addEventListener('click', replay);
+
+  inputText.value = '';
 }
 
 function checkAnswer() {
   let guess = Number(inputText.value);
-  console.log(guess === randomNum);
-  console.log(typeof guess);
+  
+  if (guess === randomNum) {
+    //success
+    resultMsg.innerHTML = 'Congratulations! You got it right!';
+    resultMsg.style.backgroundColor = 'mediumseagreen';
+    lowOrHigh.innerHTML = '';
+  } else {
+    guesses.push(' ' + guess);
+    preGuesses.innerHTML = 'Previous guesses: ';
+    preGuesses.innerHTML += guesses;
+    resultMsg.innerHTML = 'Wrong!';
+    resultMsg.style.backgroundColor = 'salmon';
+    if (guess < randomNum) {
+      //too low
+      lowOrHigh.innerHTML = 'Last guess was too low!';
+    } else if (guess > randomNum) {
+      //too high
+      lowOrHigh.innerHTML = 'Last guess was too high!';
+    }
+  }  
+  inputText.value = '';
 }
 
 function replay() {
